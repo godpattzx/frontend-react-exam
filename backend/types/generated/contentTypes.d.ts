155a9ec29ff7aa42d02373543d5dd9d178d735dd
พชรพล ศุกลสกุล <patt502090@hotmail.com>
2024-01-26 10:45:00 +0700

    commit

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -362,6 +362,32 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiBookBook extends Schema.CollectionType {
+  collectionName: 'books';
+  info: {
+    singularName: 'book';
+    pluralName: 'books';
+    displayName: 'book';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    price: Attribute.Integer;
+    description: Attribute.String;
+    author: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::book.book', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::book.book', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -791,6 +817,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::book.book': ApiBookBook;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [books, setBooks] = useState(null);
+  const [priceS, setPriceS] = useState(0);
+  const [priceF, setPriceF] = useState(10000);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:1337/api/books?populate=*&filters[price][$gte]=${priceS}&filters[price][$lte]=${priceF}`)
+      .then((response) => {
+        console.log("Data from API:", response.data);
+        setBooks(response.data.data);
+      })
+      .catch((error) => console.error("Error fetching books:", error));
+  }, [priceS, priceF]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="search-input mb-3">
+        <input
+          type="number"
+          placeholder="Price Start"
+          onChange={(e) => setPriceS(e.target.value)}
+          value={priceS}
+        />
+      </div>
+      <div className="search-input mb-3">
+        <input
+          type="number"
+          placeholder="Price Final"
+          onChange={(e) => setPriceF(e.target.value)}
+          value={priceF}
+        />
+      </div>
+      <Table className="table table-striped table-hover table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>Title</th>
+            <th>Detail</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((data) => (
+              <tr key={data.id}>
+                <td>{data.attributes.title}</td>
+                <td>{data.attributes.description}</td>
+                <td>{data.attributes.price}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
